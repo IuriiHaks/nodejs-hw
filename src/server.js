1536080148890dd8ac74fdcc73f6b5553d8e3e42
Iurii <valkoiurii@gmail.com>
2025-12-02 21:27:28 +0100

    mailimg

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import 'dotenv/config';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { logger } from './middleware/logger.js';
@@ -27,6 +28,7 @@ app.use(cookieParser());
 // Routes
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
 // Обробка невизначених маршрутів
 app.use(notFoundHandler);
